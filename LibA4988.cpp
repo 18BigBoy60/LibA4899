e44b8f4bf2482f6a6b4a1b5c56ef66ea64fbf83e
18BigBoy60 <Guzhenkov.Kirill@yandex.ru>
2020-12-28 11:52:37 +1000

    without acceleration

--- LibA4988.cpp
+++ LibA4988.cpp
@@ -1,5 +1,5 @@
 #include "Arduino.h"
-#include "lib_A4988.h"
+#include "LibA4988.h"
 
 LibA4988::LibA4988(int 1A, int 2A, int 1B, int 2B,int step, int ms1, int ms2, int ms3, int en = -1, int dir = 1){
 		_1A = 1A;
@@ -22,10 +22,13 @@ LibA4988::LibA4988(int 1A, int 2A, int 1B, int 2B,int step, int ms1, int ms2, in
 		pinMode(_en, OUTPUT);
 		_dir = dir;	
 		pinMode(_dir, OUTPUT);
+		stepSplit();
+		direction();
 }
 
 
 void LibA4988::stepSplit(int s = 1){
+	_st = s;
 	if (s == 1){
 		digitalWrite(_ms1, 0);
 		digitalWrite(_ms2, 0);
@@ -53,11 +56,52 @@ void LibA4988::stepSplit(int s = 1){
 	}
 }
 
+
 void LibA4988::direction(int n = 1){
 	if (n == 1){
 		digitalWrite(_dir, 1)
 	}
-	if{
+	if (n == -1){
 		digitalWrite(_dir, 0);
 	}
 }
+
+
+void LibA4988::goTo(int x_n){
+	_x_n = x_n;
+	if (_x_n < 0){
+		direction(-1);
+	}
+	if (_x_n > 0){
+		direction(1);
+	}
+	for(int x = 0; x < abs(_x_n); x++) {
+		digitalWrite(_step, HIGH);
+		delayMicroseconds(1000);
+		digitalWrite(_step, LOW);
+		delayMicroseconds(1000);
+	}
+}
+
+
+void setZeroPosition(){
+	_x_0 = _x_s;
+	_x_s = 0;
+}
+
+
+void setDegree(int degree){
+	int x = (degree / 1.8) * _st;
+	for(x = 0; x < abs(_x_n); x++) {
+		digitalWrite(_step, HIGH);
+		delayMicroseconds(1000);
+		digitalWrite(_step, LOW);
+		delayMicroseconds(1000);
+	}
+}
+
+int LibA4988::xNow(){
+	return _x_s;
+}
+
+
--- LibA4988.h
+++ LibA4988.h
@@ -1,15 +1,19 @@
-#ifndef Morse_h
-#define Morse_h
+#ifndef LibA4988
+#define LibA4988
  
-#include "WProgram.h"
 #include "Arduino.h"
+
 class LibA4988{
 	public:
 		LibA4988(int 1A, int 2A, int 1B, int 2B,int step, int ms1, int ms2, int ms3, int en = -1, int dir = 1);
 		void stepSplit(int s);
 		void direction(int n);
+		void xNow();
+		void goTo(int x_n);
+		void setZeroPosition();
+		void setDegree(int degree);
 	private:
 		int _1A, _2A, _1B, _2B, _step, _ms1, _ms2, _ms3, _en, _dir;
-		int x_0, x_n;
+		int _x_0, _x_n, _x_s, _st;
 }
 #endif
